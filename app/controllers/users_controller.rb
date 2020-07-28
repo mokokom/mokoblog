b3e355e7cb5dc154f9b89891fbf69c06722d0d6b
Raphael Delorme <mokokebeloke@gmail.com>
2020-07-28 20:16:41 +0200

    add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: %i[edit update]
 	def new
 		@user = User.new
 	end
@@ -14,20 +15,27 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update(user_params)
 			flash[:notice] = "Hey #{@user.username}, your account has been successfully updated"
 			redirect_to articles_path
 		else
-			render :new
+			render :edit
 		end
 	end
 
+	def show
+		@user = User.find(params[:id])
+		@articles = @user.articles
+	end
+
 	private
+
+	def set_user
+		@user = User.find(params[:id])
+	end
 
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
