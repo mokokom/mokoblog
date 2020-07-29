f16390234d6d5eb44656082dda9a831e2aa35f4e
Raphael Delorme <mokokebeloke@gmail.com>
2020-07-29 19:37:16 +0200

    add authentication helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:notice] = "Welcome to the mokoblog #{@user.username}, your account has been successfully created"
 			redirect_to users_path
 		else
