2d27ab86f967653c85f275330088f93523d9f28e
Raphael Delorme <mokokebeloke@gmail.com>
2020-08-01 12:40:43 +0200

    add many-to-many backend relation between article and category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
+	has_many :article_categories
+	has_many :articles, through: :article_categories
 	validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 25, minimum: 3 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_180331) do
+ActiveRecord::Schema.define(version: 2020_08_01_102157) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
