d75e8dc248f59940ce3bcac2f41b71da97714f93
Raphael Delorme <mokokebeloke@gmail.com>
2020-08-02 16:49:42 +0200

    style general layout and navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
 	end
 
 	def index
-		@users = User.paginate(page: params[:page], per_page: 5)
+		@users = User.paginate(page: params[:page], per_page: 7)
 	end
 
 	def destroy
